bbef63af5decfdb2b5d55057e10ec08a11cd2b33
yogisen <36984500+yogisen@users.noreply.github.com>
2018-03-02 19:03:56 +0100

    Delete VoitureJamesBond.java

--- VoitureJamesBond.java
+++
@@ -1,9 +0,0 @@
-public class VoitureJamesBond extends Voiture {
-
-    public int rouler(int duree) {
-        int distance = duree * 180;
-        System.out.println(":=D YAOUHHHHH !!! Je FONCE sur une distance de "+distance+" .");
-        return distance;
-    }
-    
-}
