095406816fdc8f8b34eb1a6e01e4ed639f697f62
yogisen <36984500+yogisen@users.noreply.github.com>
2018-03-07 13:24:46 +0100

    Add files via upload

--- exo/W2/ManipRectangle.java
+++ exo/W2/ManipRectangle.java
@@ -1,21 +1,9 @@
 	class ManipRectangle {
-		
-		public static void main(String[] args) {
-			
-			Rectangle rect = new Rectangle(1.5, 12.8);
-			System.out.println("Surface : " + rect.surface());
-
-			rect.largeur = 3.2;
-			rect.longueur = 6.9;
-			System.out.println("Surface : " + rect.surface());
-		}
+public static void main(String[] args) {
+Rectangle rect = new Rectangle(1.5,12.8);
+System.out.println("Surface : " + rect.surface());
+rect.setLargeur(3.2);
+rect.setLongueur(6.9);
+System.out.println("Surface : " + rect.surface());
 }
-	
-	/*Supposons maintenant que nous souhaitions modifier les valeurs des attributs dans le programme principal:
-	
-	Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
-	The field Rectangle.largeur is not visible
-	The field Rectangle.longueur is not visible
-
-	at ManipRectangle.main(ManipRectangle.java:8)
-	*/
+}
--- exo/W2/Rectangle.java
+++ exo/W2/Rectangle.java
@@ -1,16 +1,32 @@
 class Rectangle {
-	
-	
-private double largeur;
-private double longueur;
 
-public Rectangle(double uneLargeur, double uneLongueur){
+	private double largeur;
+	private double longueur;
 
-	largeur = uneLargeur;
-	longueur = uneLongueur;
+	public Rectangle(double uneLargeur, double uneLongueur){
+		largeur = uneLargeur;
+		longueur = uneLongueur;
+		}
+	
+		public double surface() {
+			return largeur * longueur;
+										}
+		public double getLongueur() {
+			return longueur;
 }
-
-	public double surface() {
-		return largeur * longueur;
-	}
+		public double getLargeur() {
+			return largeur;
+}
+		public void setLargeur(double l) {
+			if (l < 0) {
+					l = -l;
+								}
+			largeur = l;
+}
+		public void setLongueur(double l) {
+			if (l < 0) {
+				l = -l;
+			}
+			longueur = l;
+		}
 }
