58d16f858c8cd7707ae9ec0a4846eddbe655df80
yogisen <36984500+yogisen@users.noreply.github.com>
2018-03-07 12:02:50 +0100

    Add files via upload

--- exo/Magicien.java
+++ exo/Magicien.java
@@ -1,4 +1,31 @@
 
-public class Magicien {
-
+class Magicien {
+// ce que le magicien doit deviner:
+private int ageDevine;
+private int argentDevine;
+// pour faire son tour, le magicien a besoin d'au moins
+// un spectateur et d'un assistant
+public void tourDeMagie(Assistant fidele, Spectateur quidam) {
+Papier billet = new Papier();
+System.out.println("[Magicien] un petit tour de magie...");
+// le magicien donne ses instructions :
+quidam.ecrire(billet);
+fidele.lire(billet);
+fidele.calculer();
+calculer(fidele.annoncer());
+annoncer();
+}
+// partie privée ici car seul le magicien sait ce qu'il doit
+// faire dans son tour
+private void calculer(int resultatRecu) {
+resultatRecu += 115;
+ageDevine = resultatRecu / 100;
+argentDevine = resultatRecu % 100;
+}
+private void annoncer() {
+System.out.println("[Magicien] "
++ " - hum... je vois que vous êtes agé de "
++ ageDevine + " ans ");
+System.out.println(" et que vous avez " + argentDevine + " francs suisses en poche !");
+}
 }
--- exo/Spectateur.java
+++ exo/Spectateur.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 
-public class Spectateur {
-	
-	private final static Scanner clavier = new Scanner(System.in);
-
-	int age;
-	int argent;
-	
-	
+class Spectateur {
+// pour le moment a prendre comme tel (vu dans le MOOC précédent)
+// nous y reviendrons dans ce cours:
+private final static Scanner clavier = new Scanner(System.in);
+// Les spécificités du spectateur
+private int age;
+private int argent;
+// lorsqu'il entre dans la salle (avant il n'existe pas pour nous)
+public void arriver() {
+System.out.println("[Spectateur] (j'entre en scène)");
+System.out.print("Quel âge ai-je ? ");
+age = clavier.nextInt();
+do {
+System.out.print("Combien d'argent ai-je en poche (<100) ? ");
+argent = clavier.nextInt();
+} while (argent >= 100);
+System.out.println("[Spectateur] (j'ai un montant qui convient)");
+}
+/* Dans cette modélisation on suppose que le papier
+n'appartient à personne : il a été par exemple trouvé
+dans la salle de spectacle
+*/
+// écrit sur un papier
+public void ecrire(Papier billet) {
+System.out.println("[Spectateur] (j'écris le papier)");
+billet.ecrire(age, argent);
+}
 }
