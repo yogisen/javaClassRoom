b00e37498ff748e0c9541953c66281918035592b
yogisen <36984500+yogisen@users.noreply.github.com>
2018-03-07 13:11:45 +0100

    Add files via upload

--- exo/W2/ManipRectangle.java
+++ exo/W2/ManipRectangle.java
@@ -1,11 +1,14 @@
-
-public class ManipRectangle {
-
-	public static void main(String[] args) {
+	class ManipRectangle {
 		
-		Rectangle rect = new Rectangle(1.5,12.8);
-		System.out.println("Surface : " + rect.surface());
-		
-	}
+		public static void main(String[] args) {
+			
+			Rectangle rect = new Rectangle(1.5, 12.8);
+			System.out.println("Surface : " + rect.surface());
 
+			rect.largeur = 3.2;
+			rect.longueur = 6.9;
+			System.out.println("Surface : " + rect.surface());
+		}
 }
+	
+	//Supposons maintenant que nous souhaitions modifier les valeurs des attributs dans le programme principal:
